433317f25b75220bab25f11f5b83824097d5bbd1
yjlee06 <yongjealee06@gmail.com>
2025-06-28 02:15:31 +0900

    vocab.py - special_token을 다룰 수 있도록 변경 loader.py - 해당 파일에 vocab_loader 클래스와 load_file 메서드 추가 필요

--- jamo_tokenizer_py/vocab.py
+++ jamo_tokenizer_py/vocab.py
@@ -1,3 +1,4 @@
+import inspect
 from time import sleep
 
 from util.logger import Log
@@ -6,15 +7,14 @@ from util.logger import Log
 class Vocab:
     __token_dict: dict[str, int]
     __id_dict: dict[int, str]
+    __special_token_dict: dict[str, int]
 
     __log: Log
-    pad: [str, int]
-    unk: [str, int]
-    mask: [str, int]
+    pad: [str, int] # default token
+    unk: [str, int] # default token
 
-    def __init__(self, vocab_dict: dict[str, int] = None,
-                 padding_token: int = None, unknown_token: int = None,
-                 masking_token: int = None, log: Log = Log):
+    def __init__(self, vocab_dict: dict[str, int] = None, special_token_list: list[int] = None,
+                 padding_token: int = None, unknown_token: int = None, log: Log = Log):
 
         self.__log = log
 
@@ -24,11 +24,22 @@ class Vocab:
             self.__token_dict = vocab_dict
             self.__id_dict = {v: k for k, v in vocab_dict.items()}
 
-        self.pad = [padding_token, self.__id_dict[padding_token]]
-        self.unk = [unknown_token, self.__id_dict[unknown_token]]
-        self.mask = [masking_token, self.__id_dict[masking_token]]
+        # pad 토큰이나 unk 토큰이 매개변수로 입력되지 않았거나 기존 vocab에 존재하지 않을 경우
+        if not padding_token or not self.__id_dict.get(padding_token):
+            self.__log.exit_log( "vocab.py",inspect.currentframe().f_code.co_name,"padding token must be set.")
 
+        if not unknown_token or not self.__id_dict.get(unknown_token):
+            self.__log.exit_log("vocab.py", inspect.currentframe().f_code.co_name, "unknown token must be set.")
 
+        self.pad = [padding_token, self.__id_dict.get(padding_token)]
+        self.unk = [unknown_token, self.__id_dict.get(unknown_token)]
+
+        # 커스텀 스페셜 토큰을 __special_token_dict에 저장
+        for token_id in special_token_list:
+            t = self.get_token(token_id)
+            if self.unk[0] == t: # 토큰이 vocab에 저장되어 있지 않을 경우
+                self.__log.exit_log("vocab.py", inspect.currentframe().f_code.co_name, "special_token must be include in vocab_dict.")
+            self.__special_token_dict[token_id] = t
 
     def get_id(self, token: str):
         t = self.__token_dict.get(token)
@@ -44,6 +55,8 @@ class Vocab:
         else:
             return t
 
+    def special_token(self) -> dict[str, int]:
+        return self.__special_token_dict
+
     def get_vocab(self) -> dict[str, int]:
         return self.__token_dict
-
